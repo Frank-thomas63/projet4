9c0fec5a9c43d3fca06680ef3f801642c62e83ad
Frank-thomas63 <franktcontact@gmail.com>
2019-09-14 11:08:51 +0200

    last maj

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ btn.onclick = function(){
 const burger = document.querySelector('.burger');
 var nav = document.querySelector('.aftermenu');
 var nav1 = document.querySelector('.aftermenu1');
+//var hover = getComputedStyle(document.querySelector('.burger span'),':before');
+var hover = document.querySelector('data-before');
+burger.addEventListener('mouseover', () => {
+// alert('ca passe');
+  //burger.classList.toggle('start');
+   hover.style.top= 10+'px';
+//hover.style.backgroundColor = 'Green';
+});
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
